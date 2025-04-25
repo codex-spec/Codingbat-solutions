45d9289261d92e1b08682cf63cf5f18a2d3a7007
A Sarvari <44910985+codex-spec@users.noreply.github.com>
2025-04-25 05:46:03 -0400

    Create countHi.java

---
+++ java/String-2/countHi.java
@@ -0,0 +1,13 @@
+// Return the number of times that the string "hi" appears anywhere in the given string.
+
+// countHi("abc hi ho") → 1
+// countHi("ABChi hi") → 2
+// countHi("hihi") → 2
+
+public int countHi(String str) {
+  int c = 0;
+  for(int i = 0; i < str.length()-1; i++) {
+    if(str.substring(i,i+2).equals("hi")) c++;
+  }
+  return c;
+}
